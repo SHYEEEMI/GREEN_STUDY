4f78f1c1eef4de98f69a66e1ba5c9c3baa1cbc8c
hyemi01 <103048682+hyemi01@users.noreply.github.com>
2022-05-03 16:34:40 +0900

    Add files via upload

---
+++ wix/index.php
@@ -0,0 +1,71 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wix</title>
+    <link rel="stylesheet" href="css.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="logo">
+                <img src="image/logo.svg">
+            </div>
+            <div class="nav">
+                <ul>
+                    <li>기능</li>
+                    <li>둘러보기</li>
+                    <li>프리미엄</li>
+                    <li>템플릿</li>
+                    <li>문의하기</li>
+                </ul>
+            </div>
+            <div class="sign">
+                <span>로그인</span>
+                <span>회원가입</span>
+            </div>
+        </div>
+    </header>
+
+    <main>
+    <div class="video">
+        <video muted autoplay loop>
+            <source src="image/movie.mp4" type="video/mp4">
+            <strong>Your browser does not support the video tag.</strong>
+        </video>
+    </div> 
+    <div class="text">
+        <h1>전문적인 웹사이트 만들기</h1>
+        <h2>내가 원하는 방식으로 웹 존재를 생성, 디자인, 관리 및 개발 할 수 있는 자유를 제공 하는 플랫폼을 경험하세요.</h2>
+        <span>시작하기</span>
+    </div>
+    <div class="image">
+        <div class="img1">
+            <img src="image/image1.png">
+        </div>
+        <div class="img2">
+            <img src="image/image2.png">
+        </div>
+        <div class="img3">
+            <img src="image/image3.png">
+        </div>  
+    </div>
+    </main>
+    <div class="content">
+        <div class="con">
+            <h1>무엇이든 제작<br>가능한 자유로움</h1>
+            <h1>처음 홈페이지를 제작하는 초보자부터 다년간 작업해 온 디자이너까지 전문 홈페이지 제작 도구와 함께라면 모든 것이 가능합니다. 
+                Wix에서 홈페이지를 제작하는 방법은 크게 2가지(Wix ADI & Wix 에디터)가 있습니다. 
+                인공지능 기술에 기반을 둔 Wix ADI를 선택하면 자동으로 제작된 맞춤형 홈페이지를 만나볼 수 있습니다. 
+                Wix 에디터의 경우 직접 홈페이지를 원하는 대로 디자인할 수 있습니다. 홈페이지에 보다 다양한 고급 기능을 추가하고 싶다면 Velo by Wix를 확인해보세요. 
+                무료 홈페이지 제작 도구 Wix가 도와드리겠습니다.</h1>
+        </div>
+    </div>
+   
+</body>
+</html>
